f823dc2524b8eef7d93f964e72cc2f7e2dbe08d6
Carson274 <carsonsecrest@gmail.com>
2024-02-15 20:45:45 -0800

    Implemented navigation to landing page by clicking the navbar logo and designed the majority of the login page UI

--- frontend/src/components/NavComponent.vue
+++ frontend/src/components/NavComponent.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import { useRouter } from 'vue-router';
+
+// get access to the router
+const router = useRouter();
+
+// method to navigate back to the landing page whenever the logo is clicked
+const navigateToLanding = () => {
+  router.push('/');
+};
 </script>
 
 <template>
     <div class="navbar">
         <div id="logo_section">
-            <img src="../assets/QuickTailor_Logo.svg" alt="Name" id="navbar_logo"/>
-            <h3>uickTailor</h3>
+            <img @click="navigateToLanding" src="../assets/QuickTailor_Logo.svg" alt="Name" id="navbar_logo"/>
+            <h3 @click="navigateToLanding">uickTailor</h3>
         </div>
     </div>
 </template>
@@ -14,11 +23,14 @@ import { Button } from '@/components/ui/button'
 <style scoped>
 .navbar {
     width: 100%;
-    height: 10vh;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    background-color: transparent;
+    position: fixed;
+    top: 30px;
+    left: 40px;
 }
 
 #logo_section {
@@ -34,8 +46,16 @@ import { Button } from '@/components/ui/button'
     transform: translate(-10px, 8px);
 }
 
+#logo_section h3:hover {
+    cursor: pointer;
+}
+
 #navbar_logo {
     width: 72px;
     height: 72px;
+}
+
+#navbar_logo:hover {
+    cursor: pointer;
 }
 </style>
