fb0dd8934949824f58fb3a65d3031af4c6f722be
sebat2004 <sebastiantorresola3@gmail.com>
2024-02-18 05:25:49 -0800

    Connected Django to AWS Postgres DB and added default API route

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for my_app project.
+URL configuration for api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index)
 ]
