9c100868df53cfaa91d7165d7f48ab7c199bcec9
Seba <sebastiantorresola3@gmail.com>
2024-03-22 12:16:49 -0700

    Fixed screen not taking up 100% view height

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,13 @@
       <!-- route components are rendered here -->
       <router-view />
     </div>
-  </template>
+</template>
 
 <style scoped>
+div {
+    height: 100vh;
+    width: 100vw;
+    margin: 0px;
+    padding: 0px;
+}
 </style>
